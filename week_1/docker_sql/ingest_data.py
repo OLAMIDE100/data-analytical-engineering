980b05b1baaf7b2b533c31d7a6455bc4926f023c
Adesoba Olamide <adesoba5@gmail.com>
2022-01-23 18:13:00 +0100

    week tasks completed with sql scripts for homework

--- week_1/docker_sql/ingest_data.py
+++ week_1/docker_sql/ingest_data.py
@@ -5,25 +5,38 @@ from sqlalchemy import  create_engine
 import os
 
 def main(params):
+    # stating the argument
     user = params.user
     password = params.password
     host = params.host
     port = params.port
     db = params.db
-    table = params.table
-    url = params.url
+    table_1 = params.table_1
+    table_2 = params.table_2
+    url_1 = params.url_1
+    url_2 = params.url_2
 
-    csv_name = 'output.csv'
-
-    os.system(f"wget {url} -O {csv_name}")
-
+    csv_name_1 = 'output_1.csv'
+    csv_name_2 = 'output_2.csv'
+    
+    #Downloading the various tables for our database
+    os.system(f"wget {url_1} -O {csv_name_1}")
+    os.system(f"wget {url_2} -O {csv_name_2}")
+    
+    #Connecting the database
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     engine.connect
+    
+    #Reading and populating the database
+    df_2 = pd.read_csv(csv_name_2)
+    df_2.head(0).to_sql(name=table_2, con=engine, if_exists='replace')
+    df_2.to_sql(name=table_2, con=engine, if_exists='append')
+    
 
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    df_iter = pd.read_csv(csv_name_1, iterator=True, chunksize=100000)
     df = next(df_iter)
-    df.head(n=0).to_sql(name=table, con=engine, if_exists='replace')
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    df.head(n=0).to_sql(name=table_1, con=engine, if_exists='replace')
+    df_iter = pd.read_csv(csv_name_1, iterator=True, chunksize=100000)
 
 
     while True: 
@@ -34,11 +47,13 @@ def main(params):
         df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
         df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
         
-        df.to_sql(name=table, con=engine, if_exists='append')
+        df.to_sql(name=table_1, con=engine, if_exists='append')
 
         t_end = time()
 
         print('inserted a new chunk into the table in {} seconds'.format(t_end - t_start))
+
+   
 
 
 if __name__ == '__main__':
@@ -51,8 +66,10 @@ if __name__ == '__main__':
     parser.add_argument('--host',help="host for postgres")
     parser.add_argument('--port',help="port for postgres")
     parser.add_argument('--db',help="databasename ")
-    parser.add_argument('--table',help="tablename")
-    parser.add_argument('--url',help="url for the downloaded dataset")
+    parser.add_argument('--table_1',help="tablename")
+    parser.add_argument('--url_1',help="url for the downloaded dataset")
+    parser.add_argument('--table_2',help="tablename")
+    parser.add_argument('--url_2',help="url for the downloaded dataset")
 
     args = parser.parse_args()
     
